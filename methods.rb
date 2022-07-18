2e6079a5eef1696447a899d132590a3afe2a4a3b
Nathaniel Davydov <nathanielmdavydov@gmail.com>
2022-07-18 13:16:37 -0400

    commit tree 261227d82f52aeef462b02abb99e2c8eed897733

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
 # Your code here!
+def greetProgrammer
+    puts "Hello, programmer!"
+  end
+
+  def greet(Nathaniel)
+    puts "Hello, #{Nathaniel}"
+  end
+
+  def greetWithDefault(name = "programmer")
+    put "Hello, #{name}!"
+  end
+
+  def add_and_log(num1, num2)
+    puts num1 + num2
+  end
+
+  def add_and_return(num1, num2)
+    return num1 + num2
+  end
